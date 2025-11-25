39c0ceccc36008c27cd3b4e5ab10166a3e53200d
gianluca-salducco <gianluca.salducco@gmail.com>
2025-11-25 20:49:56 +0100

    feat: update Storybook configuration and add accessibility addon; enhance typography styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,15 @@
 import type { Preview } from "@storybook/react-vite";
+import { INITIAL_VIEWPORTS } from "storybook/viewport";
 import "../src/styles";
 
 const preview: Preview = {
   parameters: {
+    viewport: {
+      options: INITIAL_VIEWPORTS,
+    },
+    initialGlobals: {
+      viewport: { value: "mobile1", isRotated: false },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
